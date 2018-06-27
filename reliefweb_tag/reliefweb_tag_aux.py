ed088234ba5ebf83388a6c32372bea5c8b47cbf6
hernandezrivera <hernandez@reliefweb.int>
2018-06-27 12:26:53 -0400

    texting empty requirements and minor change on aux

--- reliefweb_tag/reliefweb_tag_aux.py
+++ reliefweb_tag/reliefweb_tag_aux.py
@@ -71,14 +71,31 @@ def normalize2(text):
     cucco_config = Config()
     cucco_config.language = detect_language(text)
 
-    if (cucco_config.language in ('es', 'en', 'fr')):
+    # remove numbers
+    #numbers_regex= '(\d+)([,.]*\d*)+'
+    #symbols_regex = '[(’\s*)”“]'
+    #regex = '[(' + numbers_regex + ')(' + symbols_regex + ')]'
+    text = re.sub('(\d+)([,.]*\d*)+', '',text)
+    #text = re.sub(regex, '',text)
+
+
+    if (cucco_config.language in ('en')):
         cucco = Cucco(config=cucco_config)
         normalizations = [
             'remove_stop_words',
-            # 'remove_accent_marks', # french accents
-            ('replace_hyphens', {'replacement': ' '}),
-            ('replace_symbols', {'replacement': ' '}),
-            ('replace_punctuation', {'replacement': ' '}),
+            #'remove_accent_marks', # french accents, not needed in english
+            # ('replace_symbols', {'replacement': ' '}), #very expensive, character by character - no aparent diff
+            ('replace_hyphens', {'replacement': ' '}), # don't replace ’ as hyphnen            ('replace_punctuation', {'replacement': ' '}),
+            'remove_extra_white_spaces',
+        ]
+    elif (cucco_config.language in ('es','fr')):
+        cucco = Cucco(config=cucco_config)
+        normalizations = [
+            'remove_stop_words',
+            'remove_accent_marks', # french accents and spanish enies replaced by regular letter
+            #('replace_hyphens', {'replacement': ' HYPHEN '}), # not needed in FR and ES
+            # ('replace_symbols', {'replacement': ' SYMBOL '}), # removes spanish accents, to except those characters
+            ('replace_punctuation', {'replacement': ' '}), #dont remove ” “
             'remove_extra_white_spaces',
         ]
     else:
@@ -87,20 +104,20 @@ def normalize2(text):
             # 'remove_stop_words', -- not an identified language
             # 'remove_accent_marks', # french accents
             ('replace_hyphens', {'replacement': ' '}),
-            ('replace_symbols', {'replacement': ' '}),
+            # ('replace_symbols', {'replacement': ' '}),
             ('replace_punctuation', {'replacement': ' '}),
             'remove_extra_white_spaces',
         ]
 
     text = cucco.normalize(text, normalizations)
 
-    text = re.sub('(\d+)%', '%', text)  # convert numbers percent to %
-    text = re.sub('(\d+)', '#', text)  # convert numbers to #
+    # text = re.sub('(\d+)%', '%', text)  # convert numbers percent to %
+    # text = re.sub('(\d+)', '#', text)  # convert numbers to #
     # text = re.sub('#(?P<word>([a-zA-Z])+)', '\g<word>', text) # remove numbers before and after strings'
     # text = re.sub('(?P<word>([a-zA-Z])+)#', '\g<word>', text) # remove numbers before and after strings'
-    text = text.split()
+    # text = text.split()
     # text = [w for w in text if ( len(w) > 2 and len(w) < 20 ) ] # remove short and very long words
-    text = ' '.join(text)
+    # text = ' '.join(text)
 
     return text
 
